b4ed13e52bb487b8469a8a2314a98273f4fd7923
JBearCode <joelbmoran@gmail.com>
2021-05-19 12:45:49 -0400

    Add more dishes to menu using factory function

--- src/module-home.js
+++ src/module-home.js
@@ -1,48 +1,6 @@
 const loadHome = () => {
     // get main div
     const contentDiv = document.getElementById("content");
-    
-    /* load header
-    const homeHeader = document.createElement("header");
-    contentDiv.appendChild(homeHeader);
-    
-    const restaurantNameDiv = document.createElement("div");
-    restaurantNameDiv.id = "restaurant-name-div";
-    homeHeader.appendChild(restaurantNameDiv);
-    
-    const h1 = document.createElement("h1");
-    h1.textContent = "Rice & Spice";
-    
-    const h2 = document.createElement("h2");
-    h2.textContent = "Authentic Lao Cuisine";
-    
-    restaurantNameDiv.appendChild(h1);
-    restaurantNameDiv.appendChild(h2);
-
-    // load nav
-    const nav = document.createElement("nav");
-    homeHeader.appendChild(nav);
-
-    const buttonHome = document.createElement("button");
-    buttonHome.setAttribute = ('type', 'button');
-    buttonHome.className = "active";
-    buttonHome.id = "home-button";
-    buttonHome.textContent = "Home";
-
-    const buttonMenu = document.createElement("button");
-    buttonMenu.setAttribute = ('type', 'button');
-    buttonMenu.id = "menu-button";
-    buttonMenu.textContent = "Menu";
-
-    const buttonLocations = document.createElement("button");
-    buttonLocations.setAttribute = ('type', 'button');
-    buttonLocations.id = "locations-button";
-    buttonLocations.textContent = "Locations";
-
-    nav.appendChild(buttonHome);
-    nav.appendChild(buttonMenu);
-    nav.appendChild(buttonLocations);
-    */
 
     // load banner image div and img
     const bannerImageDiv = document.createElement("div");
--- src/module-menu.js
+++ src/module-menu.js
@@ -1,49 +1,6 @@
 const loadMenu = () => {
     // get main div
     const contentDiv = document.getElementById("content");
-    
-    /* load header
-    const homeHeader = document.createElement("header");
-    contentDiv.appendChild(homeHeader);
-    
-    const restaurantNameDiv = document.createElement("div");
-    restaurantNameDiv.id = "restaurant-name-div";
-    homeHeader.appendChild(restaurantNameDiv);
-    
-    const h1 = document.createElement("h1");
-    h1.textContent = "Rice & Spice";
-    
-    const h2 = document.createElement("h2");
-    h2.textContent = "Authentic Lao Cuisine";
-    
-    restaurantNameDiv.appendChild(h1);
-    restaurantNameDiv.appendChild(h2);
-
-    // load nav
-    const nav = document.createElement("nav");
-    homeHeader.appendChild(nav);
-
-    const buttonHome = document.createElement("button");
-    buttonHome.setAttribute = ('type', 'button');
-    buttonHome.id = "home-button";
-    buttonHome.textContent = "Home";
-    buttonHome.classList.remove("active");
-
-    const buttonMenu = document.createElement("button");
-    buttonMenu.setAttribute = ('type', 'button');
-    buttonMenu.className = "active";
-    buttonMenu.id = "menu-button";
-    buttonMenu.textContent = "Menu";
-
-    const buttonLocations = document.createElement("button");
-    buttonLocations.setAttribute = ('type', 'button');
-    buttonLocations.id = "locations-button";
-    buttonLocations.textContent = "Locations";
-
-    nav.appendChild(buttonHome);
-    nav.appendChild(buttonMenu);
-    nav.appendChild(buttonLocations);
-    */
 
     // load banner image div and img
     const bannerImageDiv = document.createElement("div");
@@ -87,8 +44,38 @@ const loadMenu = () => {
 
     let src1 = "../static/pho.jpg";
     let alt1 = "A bowl of pho";
-    let desc1 = "Spiced with an array of herbs and flavors, this steaming traditional dish warms the very soul.";
+    let desc1 = `Spiced with an array of herbs and flavors, 
+    this steaming traditional dish of traditional pho warms the very soul.`;
     menuFactory(src1, alt1, desc1);
+
+    let src2 = "../static/red-curry.jpg";
+    let alt2 = "Red curry";
+    let desc2 = `Your mouth isn't ready for the explosion 
+    of spicy flavor delivered by each spoonful of our famous red curry`;
+    menuFactory(src2, alt2, desc2);
+
+    let src3 = "../static/shrimp-pad-thai.jpg";
+    let alt3 = "Shrimp pad thai";
+    let desc3 = `Our shrimp pad thai is a perfect blend of sweet, sour, and spicy
+    that will leave you in breathless wonder.`;
+    menuFactory(src3, alt3, desc3);
+
+    let src4 = "../static/garlic-pork.jpg";
+    let alt4 = "Garlic pork";
+    let desc4 = `Garlic lovers travel from distant continents to experience our 
+    famous garlic pork, served with rice and topped with an egg fried to perfection`;
+    menuFactory(src4, alt4, desc4);
+
+    let src5 = "../static/smoothie.jpg";
+    let alt5 = "Smoothie";
+    let desc5 = `Cool off with a sweet smoothie made from 100% locally-sourced organic fruit`;
+    menuFactory(src5, alt5, desc5);
+
+    let src6 = "../static/mango-sticky-rice.jpg";
+    let alt6 = "Mango sticky rice";
+    let desc6 = `Once you've tasted a serving of mango sticky rice, you may find yourself
+    ordering a dozen more. Don't say we didn't warn you.`;
+    menuFactory(src6, alt6, desc6);
 }
 
 export { loadMenu };
